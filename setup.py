2789b6af469bcc3c02c3c193751d542ec5a2cd50
David Bonnes <david@bonnes.me>
2019-04-03 13:45:14 +0100

    v0.2.2 - Initial release

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/zxdavb/geniushub-client",
+    download_url = 'https://github.com/zxdavb/geniushub-client/archive/VERSION.tar.gz',
     packages=setuptools.find_packages(),
     keywords = ['genius', 'geniushub', 'heatgenius'],
     classifiers=[
