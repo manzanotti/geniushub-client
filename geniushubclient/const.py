16ba2c90f5db5c218102186875af93d4e5934372
David Bonnes <david@bonnes.me>
2019-05-26 15:59:11 +0100

    rework occupancy code 2

--- geniushubclient/const.py
+++ geniushubclient/const.py
@@ -1,16 +1,16 @@
-"""Python client library for the Genius Hub API."""
+'''Python client library for the Genius Hub API.'''
 from types import SimpleNamespace
 
 DEFAULT_TIMEOUT_V1 = 300
 DEFAULT_TIMEOUT_V3 = 20
 
 API_STATUS_ERROR = {
-    400: "The request body or request parameters are invalid.",
-    401: "The authorization information is missing or invalid.",
-    404: "No zone/device with the specified ID was found "
-         "(or the state property does not exist on the specified device).",
-    502: "The hub is offline.",
-    503: "The authorization information is invalid.",
+    400: 'The request body or request parameters are invalid.',
+    401: 'The authorization information is missing or invalid.',
+    404: 'No zone/device with the specified ID was found '
+         '(or the state property does not exist on the specified device).',
+    502: 'The hub is offline.',
+    503: 'The authorization information is invalid.',
 }
 ZONE_TYPES = SimpleNamespace(
     Manager=1,
@@ -20,12 +20,12 @@ ZONE_TYPES = SimpleNamespace(
     TPI=5,
     Surrogate=6
 )
-# 1 "Manager"
-# 2 "On / Off"
-# 3 "Radiator"
+# 1 'Manager'
+# 2 'On / Off'
+# 3 'Radiator'
 # 4
-# 5 "Hot Water Temperature" OR "Wet Underfloor"
-# 6 "Group"
+# 5 'Hot Water Temperature' OR 'Wet Underfloor'
+# 6 'Group'
 
 ZONE_MODES = SimpleNamespace(
     Off=1,
@@ -52,12 +52,12 @@ KIT_TYPES = SimpleNamespace(
     Luminance=512
 )
 KIT_SKU_TO_TEXT = {
-    "DA-WRT-C": "Room Thermostat",
-    "DA-WRV-B": "Radiator Valve",
-    "DA-WRV-C": "Genius Valve",
-    "HO-ESW-D": "Electric Switch",
-    "PH-PLG-C": "Smart Plug",
-    "PH-WRS-B": "Room Sensor",
+    'DA-WRT-C': 'Room Thermostat',
+    'DA-WRV-B': 'Radiator Valve',
+    'DA-WRV-C': 'Genius Valve',
+    'HO-ESW-D': 'Electric Switch',
+    'PH-PLG-C': 'Smart Plug',
+    'PH-WRS-B': 'Room Sensor',
 }
 ZONE_FLAGS = SimpleNamespace(
     Frost=1,
@@ -102,43 +102,33 @@ LEVEL_TO_TEXT = {
     0: 'information'
 }
 DESCRIPTION_TO_TEXT = {
-    "manager:no_boiler_controller":
-        "The hub does not have a boiler controller assigned",
-    "manager:no_boiler_comms":
-        "The hub has lost communication with the boiler controller",
-    "manager:no_temp":
-        "The hub does not have a valid temperature",
-    "manager:weather":                                                           # checked/confirmed
-        "Unable to fetch the weather data",
-    "manager:weather_data":
-        "Weather data -",
+    'manager:no_boiler_controller':
+        'The hub does not have a boiler controller assigned',
+    'manager:no_boiler_comms':
+        'The hub has lost communication with the boiler controller',
+    'manager:no_temp':
+        'The hub does not have a valid temperature',
+    'manager:weather':                                                           # checked/confirmed
+        'Unable to fetch the weather data',
+    'manager:weather_data':
+        'Weather data -',
 
-    "zone:using_weather_temp":
-        "{zone_name} is currently using the outside temperature",
-    "zone:using_assumed_temp":
-        "{zone_name} is currently using the assumed temperature",
-    "zone:tpi_no_temp":
-        "{zone_name} has no valid temperature sensor",
+    'zone:using_weather_temp':
+        '{zone_name} is currently using the outside temperature',
+    'zone:using_assumed_temp':
+        '{zone_name} is currently using the assumed temperature',
+    'zone:tpi_no_temp':
+        '{zone_name} has no valid temperature sensor',
 
-    "node:no_comms":
-        "The {device_type} has lost communication with the Hub",
-    "node:not_seen":                                                             # checked/confirmed
-        "The {device_type} in {zone_name} can not been found by the Hub",
-    "node:low_battery":                                                          # checked/confirmed
-        "The battery for the {device_type} in {zone_name} is dead and needs to be replaced",
-    "node:warn_battery":
-        "The battery for the {device_type} is low",
+    'node:no_comms':
+        'The {device_type} has lost communication with the Hub',
+    'node:not_seen':                                                             # checked/confirmed
+        'The {device_type} in {zone_name} can not been found by the Hub',
+    'node:low_battery':                                                          # checked/confirmed
+        'The battery for the {device_type} in {zone_name} is dead and needs to be replaced',
+    'node:warn_battery':
+        'The battery for the {device_type} is low',
 }
-
-# Examples of JSON/schema (order changed):
-
-# [{'iID': 0, 'strName': '32 Clift road', 'lstIssues': [{
-#     'id': 'manager:weather', 'level': 1,
-#         'data': {'msg': ''} }] }]
-
-# {'iID': 10, 'strName': 'Bathrooms', 'lstIssues': [{
-#     'id': 'node:not_seen', 'level': 2,
-#         'data': {'location': 'Bathrooms', 'nodeID': '19', 'nodeHash': ...} }] }]
 
 IDAY_TO_DAY = {
     0: 'sunday',
@@ -167,192 +157,192 @@ ATTRS_ISSUE = {
 # This is from Vendor's bower.js
 DEVICES_MODEL = [
     {
-        hash: "VIRTUAL",
-        sku: "virtual node",
-        description: "Virtual Node"
+        'hash': 'VIRTUAL',
+        'sku': 'virtual node',
+        'description': 'Virtual Node'
     }, {
-        hash: "0x0000000000000000",
-        sku: "n/a",
-        description: "Unrecognised Device"
+        'hash': '0x0000000000000000',
+        'sku': 'n/a',
+        'description': 'Unrecognised Device'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Radiator Valve",
-        deviceString: "wrv",
-        hash: "0x0000000200030005",
-        sku: "da-wrv-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Radiator Valve',
+        'deviceString': 'wrv',
+        'hash': '0x0000000200030005',
+        'sku': 'da-wrv-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Radiator Valve",
-        deviceString: "wrv",
-        hash: "0x0000000200040005",
-        sku: "da-wrv-b"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Radiator Valve',
+        'deviceString': 'wrv',
+        'hash': '0x0000000200040005',
+        'sku': 'da-wrv-b'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Dual Underfloor Receiver",
-        deviceString: "dur",
-        hash: "0x0000000220008004",
-        sku: "da-dur-a"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Dual Underfloor Receiver',
+        'deviceString': 'dur',
+        'hash': '0x0000000220008004',
+        'sku': 'da-dur-a'
     }, {
-        assignableZoneTypeIds: [1, 3, 5],
-        description: "Room Thermostat",
-        deviceString: "wrt",
-        hash: "0x0000000280100003",
-        sku: "da-wrt-c"
+        'assignableZoneTypeIds': [1, 3, 5],
+        'description': 'Room Thermostat',
+        'deviceString': 'wrt',
+        'hash': '0x0000000280100003',
+        'sku': 'da-wrt-c'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Genius Valve",
-        deviceString: "wrv",
-        hash: "0x00000002A0107FFF",
-        sku: "da-wrv-c"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Genius Valve',
+        'deviceString': 'wrv',
+        'hash': '0x00000002A0107FFF',
+        'sku': 'da-wrv-c'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Single Channel Receiver",
-        deviceString: "scr",
-        hash: "0x0000005900010003",
-        sku: "ho-scr-c"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Single Channel Receiver',
+        'deviceString': 'scr',
+        'hash': '0x0000005900010003',
+        'sku': 'ho-scr-c'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Electric Switch",
-        deviceString: "esw",
-        hash: "0x0000005900010010",
-        sku: "ho-esw-d"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Electric Switch',
+        'deviceString': 'esw',
+        'hash': '0x0000005900010010',
+        'sku': 'ho-esw-d'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Dual Channel Receiver",
-        deviceString: "dcr",
-        hash: "0x0000005900020003",
-        sku: "ho-dcr-c"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Dual Channel Receiver',
+        'deviceString': 'dcr',
+        'hash': '0x0000005900020003',
+        'sku': 'ho-dcr-c'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Temperature Sensor",
-        deviceString: "wts",
-        hash: "0x000000590002000D",
-        sku: "ho-wts-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Temperature Sensor',
+        'deviceString': 'wts',
+        'hash': '0x000000590002000D',
+        'sku': 'ho-wts-a'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Electric Switch",
-        deviceString: "esw",
-        hash: "0x0000005900020010",
-        sku: "ho-esw-d"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Electric Switch',
+        'deviceString': 'esw',
+        'hash': '0x0000005900020010',
+        'sku': 'ho-esw-d'
     }, {
-        assignableZoneTypeIds: [1, 3, 5],
-        description: "Room Thermostat",
-        deviceString: "wrt",
-        hash: "0x0000005900030001",
-        sku: "ho-wrt-b"
+        'assignableZoneTypeIds': [1, 3, 5],
+        'description': 'Room Thermostat',
+        'deviceString': 'wrt',
+        'hash': '0x0000005900030001',
+        'sku': 'ho-wrt-b'
     }, {
-        assignableZoneTypeIds: [1, 3, 5],
-        description: "Room Thermostat",
-        deviceString: "wrt",
-        hash: "0x0000005900050001",
-        sku: "ho-wrt-d"
+        'assignableZoneTypeIds': [1, 3, 5],
+        'description': 'Room Thermostat',
+        'deviceString': 'wrt',
+        'hash': '0x0000005900050001',
+        'sku': 'ho-wrt-d'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Single Channel Receiver",
-        deviceString: "scr",
-        hash: "0x0000005900050003",
-        sku: "ho-scr-d"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Single Channel Receiver',
+        'deviceString': 'scr',
+        'hash': '0x0000005900050003',
+        'sku': 'ho-scr-d'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Dual Channel Receiver",
-        deviceString: "dcr",
-        hash: "0x0000005900060003",
-        sku: "ho-dcr-d"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Dual Channel Receiver',
+        'deviceString': 'dcr',
+        'hash': '0x0000005900060003',
+        'sku': 'ho-dcr-d'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Smart Plug",
-        deviceString: "plg",
-        hash: "0x0000006000010003",
-        sku: "ev-plg-a"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Smart Plug',
+        'deviceString': 'plg',
+        'hash': '0x0000006000010003',
+        'sku': 'ev-plg-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Temperature Humidity Sensor",
-        deviceString: "ths",
-        hash: "0x0000006000010006",
-        sku: "es-ths-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Temperature Humidity Sensor',
+        'deviceString': 'ths',
+        'hash': '0x0000006000010006',
+        'sku': 'es-ths-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Motion Sensor",
-        deviceString: "wms",
-        hash: "0x0000006000020001",
-        sku: "es-wms-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Motion Sensor',
+        'deviceString': 'wms',
+        'hash': '0x0000006000020001',
+        'sku': 'es-wms-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Temperature Sensor",
-        deviceString: "wts",
-        hash: "0x00000071035D0002",
-        sku: "ls-wts-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Temperature Sensor',
+        'deviceString': 'wts',
+        'hash': '0x00000071035D0002',
+        'sku': 'ls-wts-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "CO2 Sensor",
-        deviceString: "cos",
-        hash: "0x00000081000100A0",
-        sku: "sa-cos-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'CO2 Sensor',
+        'deviceString': 'cos',
+        'hash': '0x00000081000100A0',
+        'sku': 'sa-cos-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Room Sensor",
-        deviceString: "wrs",
-        hash: "0x0000008600050002",
-        sku: "al-wrs-c"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Room Sensor',
+        'deviceString': 'wrs',
+        'hash': '0x0000008600050002',
+        'sku': 'al-wrs-c'
     }, {
-        assignableZoneTypeIds: [1],
-        description: "Gas Meter Reader",
-        deviceString: "umr",
-        hash: "0x0000009600010010",
-        sku: "nq-umr-a"
+        'assignableZoneTypeIds': [1],
+        'description': 'Gas Meter Reader',
+        'deviceString': 'umr',
+        'hash': '0x0000009600010010',
+        'sku': 'nq-umr-a'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Smart Plug",
-        deviceString: "plg",
-        hash: "0x0000013C00010001",
-        sku: "ph-plg-c"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Smart Plug',
+        'deviceString': 'plg',
+        'hash': '0x0000013C00010001',
+        'sku': 'ph-plg-c'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Room Sensor",
-        deviceString: "wrs",
-        hash: "0x0000013C00020002",
-        sku: "ph-wrs-a"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Room Sensor',
+        'deviceString': 'wrs',
+        'hash': '0x0000013C00020002',
+        'sku': 'ph-wrs-a'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Room Sensor",
-        deviceString: "wrs",
-        hash: "0x0000013C000C0002",
-        sku: "ph-wrs-b"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Room Sensor',
+        'deviceString': 'wrs',
+        'hash': '0x0000013C000C0002',
+        'sku': 'ph-wrs-b'
     }, {
-        assignableZoneTypeIds: [3, 5],
-        description: "Room Sensor",
-        deviceString: "wrs",
-        hash: "0x0000013C000D0002",
-        sku: "ph-wrs-b"
+        'assignableZoneTypeIds': [3, 5],
+        'description': 'Room Sensor',
+        'deviceString': 'wrs',
+        'hash': '0x0000013C000D0002',
+        'sku': 'ph-wrs-b'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Electric Switch",
-        deviceString: "esw",
-        hash: "0x0000013C000F0001",
-        sku: "ph-esw-b"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Electric Switch',
+        'deviceString': 'esw',
+        'hash': '0x0000013C000F0001',
+        'sku': 'ph-esw-b'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Electric Switch",
-        deviceString: "esw",
-        hash: "0x0000013C00100001",
-        sku: "ph-esw-a"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Electric Switch',
+        'deviceString': 'esw',
+        'hash': '0x0000013C00100001',
+        'sku': 'ph-esw-a'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Smart Plug",
-        deviceString: "plg",
-        hash: "0x0000013C00110001",
-        sku: "ph-plg-c"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Smart Plug',
+        'deviceString': 'plg',
+        'hash': '0x0000013C00110001',
+        'sku': 'ph-plg-c'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "In Wall Meter",
-        deviceString: "iwm",
-        hash: "0x0000013C001A0006",
-        sku: "ph-iwm-a"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'In Wall Meter',
+        'deviceString': 'iwm',
+        'hash': '0x0000013C001A0006',
+        'sku': 'ph-iwm-a'
     }, {
-        assignableZoneTypeIds: [1, 2, 3, 5, 6],
-        description: "Smart Plug",
-        deviceString: "plg",
-        hash: "0x0000015400011100",
-        sku: "po-plg-b"
+        'assignableZoneTypeIds': [1, 2, 3, 5, 6],
+        'description': 'Smart Plug',
+        'deviceString': 'plg',
+        'hash': '0x0000015400011100',
+        'sku': 'po-plg-b'
     }
 ]
