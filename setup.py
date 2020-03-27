9a16991767c84e84b00cea8d964585ae32699c92
David Bonnes <zxdavb@bonnes.me>
2020-03-27 21:58:59 +0000

    tweak setup

--- setup.py
+++ setup.py
@@ -1,26 +1,45 @@
-import setuptools
+import os
+import sys
 
-VERSION = "0.6.30"
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+VERSION = "0.6.30p1"
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
-setuptools.setup(
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our VERSION."""
+
+    def run(self):
+        tag = os.getenv("CIRCLE_TAG")
+
+        if tag != VERSION:
+            info = "Git tag: {tag} does not match the version of this pkg: {VERSION}"
+            sys.exit(info)
+
+
+setup(
     name="geniushub-client",
     version=VERSION,
-    author="David Bonnes & Geoff Soord",
+    packages=find_packages(),
+    install_requires=["aiohttp=>3.6.1"],
+    # metadata to display on PyPI
+    author="David Bonnes",
     author_email="zxdavb@gmail.com",
     description="A aiohttp-based client for Genius Hub systems",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/zxdavb/geniushub-client",
-    download_url="https://github.com/zxdavb/geniushub-client/archive/VERSION.tar.gz",
-    packages=setuptools.find_packages(),
+    download_url="{url}tarball/{VERSION}",
     keywords=["genius", "geniushub", "heatgenius"],
+    license="XXX",
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
         "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Home Automation",
     ],
+    cmdclass={"verify": VerifyVersionCommand},
 )
