f8c1795212bb67321f4d558a36d275162a85ef60
David Bonnes <zxdavb@bonnes.me>
2020-03-28 23:20:25 +0000

    a hack

--- geniushubclient/__init__.py
+++ geniushubclient/__init__.py
@@ -24,6 +24,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.insert(0, os.path.dirname(os.path.abspath(".")))
 
 try:
     from .zone import GeniusZone, natural_sort
