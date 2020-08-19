21aa228b3104c71baa0b5579e3505586e20ed567
David Bonnes <zxdavb@bonnes.me>
2020-08-20 00:20:07 +0100

    fix circleci config

--- tests/data_properties_v3_conversion/genius_zone_data_test.py
+++ tests/data_properties_v3_conversion/genius_zone_data_test.py
@@ -1,20 +1,13 @@
-"""
-    Tests for the GeniusZone class
-    """
+"""Tests for the GeniusZone class."""
 
 import unittest
 from unittest.mock import Mock
-from geniushubclient.const import (
-    ZONE_MODE,
-    ZONE_TYPE
-)
+from geniushubclient.const import ZONE_MODE, ZONE_TYPE
 from geniushubclient.zone import GeniusZone
 
 
 class GeniusZoneDataTests(unittest.TestCase):
-    """
-        Test for the GeniusZone Class, general data.
-        """
+    """Test for the GeniusZone Class, general data."""
 
     _device_id = "Device Id"
     _zone_name = "Zone Name"
@@ -38,53 +31,27 @@ class GeniusZoneDataTests(unittest.TestCase):
             "fFootprintAwaySP": 14.0,
             "iFootprintTmNightStart": 75600,
             "iProfile": 1,
-            "lstSP": [{
-                "fSP": 16.0,
-                "iDay": 0,
-                "iTm": 0
-            }, {
-                "fSP": 14.0,
-                "iDay": 0,
-                "iTm": 23400
-            }, {
-                "fSP": 20.0,
-                "iDay": 0,
-                "iTm": 59700
-            }, {
-                "fSP": 14.0,
-                "iDay": 0,
-                "iTm": 75000
-            }, {
-                "fSP": 16.0,
-                "iDay": 0,
-                "iTm": 75600
-            }
+            "lstSP": [
+                {"fSP": 16.0, "iDay": 0, "iTm": 0},
+                {"fSP": 14.0, "iDay": 0, "iTm": 23400},
+                {"fSP": 20.0, "iDay": 0, "iTm": 59700},
+                {"fSP": 14.0, "iDay": 0, "iTm": 75000},
+                {"fSP": 16.0, "iDay": 0, "iTm": 75600},
             ],
-            "objReactive": {
-                "fActivityLevel": 0.0
-            }
+            "objReactive": {"fActivityLevel": 0.0},
         },
-        "objTimer": [{
-            "fSP": 14.0,
-            "iDay": 0,
-            "iTm": -1
-        }],
-        "trigger": {
-            "reactive": 0,
-            "output": 0
-        },
+        "objTimer": [{"fSP": 14.0, "iDay": 0, "iTm": -1}],
+        "trigger": {"reactive": 0, "output": 0},
         "warmupDuration": {
             "bEnable": "true",
             "bEnableCalcs": "true",
             "fRiseRate": 0.5,
             "iLagTime": 2420,
             "iRiseTime": 300,
-            "iTotalTime": 2720
+            "iTotalTime": 2720,
         },
-        "zoneReactive": {
-            "fActivityLevel": 0
-        },
-        "zoneSubType": 1
+        "zoneReactive": {"fActivityLevel": 0},
+        "zoneSubType": 1,
     }
 
     def setUp(self):
@@ -93,19 +60,19 @@ class GeniusZoneDataTests(unittest.TestCase):
         self.hub = hub
 
     def test_raw_json_is_stored(self):
-        "Check that the raw json is set on the class"
+        """Check that the raw json is set on the class."""
 
         genius_zone = GeniusZone(self._device_id, self.raw_json, self.hub)
         self.assertEqual(genius_zone._raw, self.raw_json)
 
     def test_id_is_set_correctly_test(self):
-        "Check that the id is set on the data object"
+        """Check that the id is set on the data object."""
 
         genius_zone = GeniusZone(self._device_id, self.raw_json, self.hub)
-        self.assertEqual(genius_zone.data['id'], self._device_id)
+        self.assertEqual(genius_zone.data["id"], self._device_id)
 
     def test_name_is_set_correctly(self):
-        "Check that the name is set on the data object"
+        """Check that the name is set on the data object."""
 
         genius_zone = GeniusZone(self._device_id, self.raw_json, self.hub)
-        self.assertEqual(genius_zone.data['name'], self._zone_name)
+        self.assertEqual(genius_zone.data["name"], self._zone_name)
